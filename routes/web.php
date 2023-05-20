b62c0b09cdb50f5172c79ad2460b761ead935b9b
Vadim Lipatov <lipatovvs73@gmail.com>
2023-05-20 20:34:25 +0600

    add ssl docker2

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,11 @@ Route::get('/dashboard', function () {
 })->name('dashboard')
   ->middleware(['auth']);
 
+Route::middleware('auth')->group(function () {
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 
 require __DIR__ . '/auth.php';
